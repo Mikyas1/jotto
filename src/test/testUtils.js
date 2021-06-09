7a5e89a97dcbd71f33163bce6d15764d01a107ad
Mikyas Alemayehu <mikyasalemayehu1@gmail.com>
2021-06-09 22:05:50 +0300

    Set up Input component and storeFactory test util

--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,5 +1,12 @@
 import { ShallowWrapper } from 'enzyme';
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
+
+import rootReducer from '../reducers';
+
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+};
 
 /**
  * Return node(s) whit the given data-test attribute.
