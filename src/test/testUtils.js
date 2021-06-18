ad4c56aa91cfda73f8c36697442b21db6d3cc803
Mikyas Alemayehu <mikyasalemayehu1@gmail.com>
2021-06-18 13:57:40 +0300

    Install Redux Thunk middleware

--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,11 +1,15 @@
 import { ShallowWrapper } from 'enzyme';
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../reducers';
+import { middlewares } from '../configureStore';
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 /**
