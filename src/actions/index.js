38ce262d922b734972a5c8649981baeee9649f4f
Mikyas Alemayehu <mikyasalemayehu1@gmail.com>
2021-06-18 18:20:58 +0300

    Add Thunk for  action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,25 @@
+import { getLetterMatchCount } from '../helpers/index';
+
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
 };
 
-export const guessWord = (guessWord) => {
-  return (dispatch, getState) => {};
+export const guessWord = (guessedWord) => {
+  return (dispatch, getState) => {
+    const secretWord = getState().secretWord;
+    const lettersMatchedCount = getLetterMatchCount(guessedWord, secretWord);
+
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: {
+        guessedWord,
+        lettersMatchedCount,
+      },
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    }
+  };
 };
